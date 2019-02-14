a073df190743e605cb830a599c95d443ed24a293
Gasolaz <sarunas.slapsys@gmail.com>
2019-02-14 20:04:37 +0200

    Maven with Tomcat

---
+++ out/artifacts/StackDocMvn_Web_exploded/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sarunas
+  Date: 19.2.14
+  Time: 19.36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test</title>
+</head>
+<body>
+    <h1>First test</h1>
+</body>
+</html>
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,23 +3,34 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.swing.plaf.nimbus.State;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
-import java.util.Iterator;
 
 public class Main {
+
+    public static final String TOPICS_TOPIC = "topic";
+    public static final String SUB_TOPICS_SUB_TOPIC = "sub_topic";
+    public static final String DESCRIPTION_TOPIC_ID = "topic_id";
+    public static final String DESCRIPTION_SUB_TOPIC_ID = "sub_topic_id";
+    public static final String DESCRIPTION_DESCRIPTION = "description";
+
+    public static final String INSERT_INTO_TOPICS = "INSERT INTO topics(" + TOPICS_TOPIC + ") VALUES(?)";
+    public static final String INSERT_INTO_SUB_TOPICS = "INSERT INTO sub_topics(" + SUB_TOPICS_SUB_TOPIC + ") VALUES(?)";
+    public static final String INSERT_INTO_DESCRIPTION = "INSER INTO description(" + DESCRIPTION_TOPIC_ID + ", " +
+            DESCRIPTION_SUB_TOPIC_ID + ", " + DESCRIPTION_DESCRIPTION + ") VALUES(?, ?, ?)";
 
     static final String INSERT_INTO_DATABASE_TABLE_TOPICS = "INSERT INTO Topics(_id, DocTagId, Title, CreationDate, ViewCount, " +
             "LastEditDate, ContributorCount, ExampleCount, ExampleScore, " +
             "SyntaxHtml, ParametersHtml, RemarksHtml, IntroductionMarkdown, SyntaxMarkdown, " +
             "ParametersMarkdown, RemarksMarkdown, HelloWorldVersionsHtml) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, " +
             "?, ?, ?, ?, ?, ?, ?, ?)";
-    static Connection conn;
+    public static Connection conn;
 
-    public static Connection connect() throws SQLException {
+    private static Connection connect() throws SQLException {
         Connection conn = null;
         String url = "jdbc:sqlite:/home/sarunas/Codebaker/StackDocMvn/src/StackDoc.db";
         conn = DriverManager.getConnection(url);
@@ -36,57 +47,55 @@ public class Main {
             for(Object ms : jsonArray) {
                 JSONObject element = (JSONObject) ms;
 
-                long id = (Long) element.get("Id");
-                long docTagId = (Long) element.get("DocTagId");
-                String title = (String) element.get("Title");
-                String creationDate = (String) element.get("CreationDate");
-                long viewCount = (Long) element.get("ViewCount");
-                String LastEditDate = (String) element.get("LastEditDate");
-                long ContributorCount = (Long) element.get("ContributorCount");
-                long ExampleCount = (Long) element.get("ExampleCount");
-                long ExampleScore = (Long) element.get("ExampleScore");
-                String SyntaxHtml = (String) element.get("SyntaxHtml");
-                String ParametersHtml = (String) element.get("ParametersHtml");
-                String RemarksHtml = (String) element.get("RemarksHtml");
-                String IntroductionMarkdown = (String) element.get("IntroductionMarkdown");
-                String SyntaxMarkdown = (String) element.get("SyntaxMarkdown");
-                String ParametersMarkdown = (String) element.get("ParametersMarkdown");
-                String RemarksMarkdown = (String) element.get("RemarksMarkdown");
-                String HelloWorldVersionsHtml = (String) element.get("HelloWorldVersionsHtml");
+//                long id = (Long) element.get("Id");
+//                long docTagId = (Long) element.get("DocTagId");
+//                String title = (String) element.get("Title");
+//                String creationDate = (String) element.get("CreationDate");
+//                long viewCount = (Long) element.get("ViewCount");
+//                String LastEditDate = (String) element.get("LastEditDate");
+//                long ContributorCount = (Long) element.get("ContributorCount");
+//                long ExampleCount = (Long) element.get("ExampleCount");
+//                long ExampleScore = (Long) element.get("ExampleScore");
+//                String SyntaxHtml = (String) element.get("SyntaxHtml");
+//                String ParametersHtml = (String) element.get("ParametersHtml");
+//                String RemarksHtml = (String) element.get("RemarksHtml");
+//                String IntroductionMarkdown = (String) element.get("IntroductionMarkdown");
+//                String SyntaxMarkdown = (String) element.get("SyntaxMarkdown");
+//                String ParametersMarkdown = (String) element.get("ParametersMarkdown");
+//                String RemarksMarkdown = (String) element.get("RemarksMarkdown");
+//                String HelloWorldVersionsHtml = (String) element.get("HelloWorldVersionsHtml");
 
                 try {
                     conn = connect();
 
-                    PreparedStatement ps = conn.prepareStatement(INSERT_INTO_DATABASE_TABLE_TOPICS);
-                    ps.setLong(1, id);
-                    ps.setLong(2, docTagId);
-                    ps.setString(3, title);
-                    ps.setString(4, creationDate);
-                    ps.setLong(5, viewCount);
-                    ps.setString(6, LastEditDate);
-                    ps.setLong(7, ContributorCount);
-                    ps.setLong(8, ExampleCount);
-                    ps.setLong(9, ExampleScore);
-                    ps.setString(10, SyntaxHtml);
-                    ps.setString(11, ParametersHtml);
-                    ps.setString(12, RemarksHtml);
-                    ps.setString(13, IntroductionMarkdown);
-                    ps.setString(14, SyntaxMarkdown);
-                    ps.setString(15, ParametersMarkdown);
-                    ps.setString(16, RemarksMarkdown);
-                    ps.setString(17, HelloWorldVersionsHtml);
-                    ps.executeUpdate();
+                    Statement statement = conn.createStatement();
+                    ResultSet rs = statement.executeQuery("SELECT Title FROM Topics");
+                    while(rs.next()){
+                        System.out.println(rs.getString("Title"));
+                    }
+
+//                    PreparedStatement ps = conn.prepareStatement(INSERT_INTO_DATABASE_TABLE_TOPICS);
+//                    ps.setLong(1, id);
+//                    ps.setLong(2, docTagId);
+//                    ps.setString(3, title);
+//                    ps.setString(4, creationDate);
+//                    ps.setLong(5, viewCount);
+//                    ps.setString(6, LastEditDate);
+//                    ps.setLong(7, ContributorCount);
+//                    ps.setLong(8, ExampleCount);
+//                    ps.setLong(9, ExampleScore);
+//                    ps.setString(10, SyntaxHtml);
+//                    ps.setString(11, ParametersHtml);
+//                    ps.setString(12, RemarksHtml);
+//                    ps.setString(13, IntroductionMarkdown);
+//                    ps.setString(14, SyntaxMarkdown);
+//                    ps.setString(15, ParametersMarkdown);
+//                    ps.setString(16, RemarksMarkdown);
+//                    ps.setString(17, HelloWorldVersionsHtml);
+//                    ps.executeUpdate();
 
                 } catch (SQLException e){
                     e.printStackTrace();
-                } finally {
-                    try {
-                        if (conn != null) {
-                            conn.close();
-                        }
-                    } catch (SQLException e){
-                        System.out.println("pzda");
-                    }
                 }
             }
 
