ceb126cda21ca828bc131d55df3620761589109e
Gasolaz <sarunas.slapsys@gmail.com>
2019-02-24 12:51:35 +0200

    removed comments

--- src/main/java/CommentsClass.java
+++ src/main/java/CommentsClass.java
@@ -81,3 +81,55 @@
 //            }
 
 //            ResultSet rs = statement.executeQuery("SELECT * FROM subtopics as st INNER JOIN Topics as t WHERE t.topic='" + topic + "' AND st.sub_topic='" + search + "'");
+
+//                    PreparedStatement ps = conn.prepareStatement(INSERT_INTO_DATABASE_TABLE_TOPICS);
+//                    ps.setLong(1, id);
+//                    ps.setLong(2, docTagId);
+//                    ps.setString(3, title);
+//                    ps.setString(4, creationDate);
+//                    ps.setLong(5, viewCount);
+//                    ps.setString(6, LastEditDate);
+//                    ps.setLong(7, ContributorCount);
+//                    ps.setLong(8, ExampleCount);
+//                    ps.setLong(9, ExampleScore);
+//                    ps.setString(10, SyntaxHtml);
+//                    ps.setString(11, ParametersHtml);
+//                    ps.setString(12, RemarksHtml);
+//                    ps.setString(13, IntroductionMarkdown);
+//                    ps.setString(14, SyntaxMarkdown);
+//                    ps.setString(15, ParametersMarkdown);
+//                    ps.setString(16, RemarksMarkdown);
+//                    ps.setString(17, HelloWorldVersionsHtml);
+//                    ps.executeUpdate();
+
+//                    System.out.println(conn);
+
+//                    Statement stmt = conn.createStatement();
+//                    ResultSet rs = stmt.executeQuery("SELECT * FROM Topics");
+//                    while(rs.next()){
+//                        System.out.println(rs.getArray(1));
+//                    }
+
+//                    Statement statement = conn.createStatement();
+//                    ResultSet rs = statement.executeQuery("SELECT Title FROM Topics");
+//                    while(rs.next()){
+//                        System.out.println(rs.getString("Title"));
+//                    }
+
+//                long id = (Long) element.get("Id");
+//                long docTagId = (Long) element.get("DocTagId");
+//                String title = (String) element.get("Title");
+//                String creationDate = (String) element.get("CreationDate");
+//                long viewCount = (Long) element.get("ViewCount");
+//                String LastEditDate = (String) element.get("LastEditDate");
+//                long ContributorCount = (Long) element.get("ContributorCount");
+//                long ExampleCount = (Long) element.get("ExampleCount");
+//                long ExampleScore = (Long) element.get("ExampleScore");
+//                String SyntaxHtml = (String) element.get("SyntaxHtml");
+//                String ParametersHtml = (String) element.get("ParametersHtml");
+//                String RemarksHtml = (String) element.get("RemarksHtml");
+//                String IntroductionMarkdown = (String) element.get("IntroductionMarkdown");
+//                String SyntaxMarkdown = (String) element.get("SyntaxMarkdown");
+//                String ParametersMarkdown = (String) element.get("ParametersMarkdown");
+//                String RemarksMarkdown = (String) element.get("RemarksMarkdown");
+//                String HelloWorldVersionsHtml = (String) element.get("HelloWorldVersionsHtml");
