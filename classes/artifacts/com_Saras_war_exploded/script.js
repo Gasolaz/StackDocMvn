ce3132f1292136fab8a8d13126118b4231a8efe6
USERS-PC\Vartotojas <kgadeikis93@gmail.com>
2019-02-28 13:25:16 +0200

    test commit

---
+++ classes/artifacts/com_Saras_war_exploded/script.js
@@ -0,0 +1,38 @@
+
+
+
+window.onload = () => {
+
+    document.querySelector(".back").onclick = () => {
+        history.back();
+
+    }
+
+}
+
+let input_Value;
+
+function changed(e) {
+
+    console.log(e.target.value);
+
+     input_Value = e.target.value;
+}
+
+
+
+function clicked(e) {
+
+    fetch("http://localhost:8080/StackDocMvn_Web_exploded/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Access-Control-Origin": "*"
+        },
+        body: `pageNumber=${e.target.textContent}`
+               
+    })
+}
+
+
+
