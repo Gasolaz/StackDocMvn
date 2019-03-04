b3e58e971560767d5b716c4753b31d9da3ece3ba
Gasolaz <sarunas.slapsys@gmail.com>
2019-03-04 23:39:53 +0200

    deleting comments

--- src/main/java/DB/SubTopicDAO.java
+++ src/main/java/DB/SubTopicDAO.java
@@ -37,48 +37,9 @@ public class SubTopicDAO {
 
             long limitStart = ROWS*(pageNumber-1);
             StringBuilder sb = new StringBuilder();
-
-//            if(topic == 0 && search.trim().equals("")) {
-//                PreparedStatement ps = conn.prepareStatement(buildingSearchStatement(sb, topic, search));
-//                ps.setLong(1, limitStart);
-//                ps.setLong(2, ROWS);
-//                ResultSet rs = ps.executeQuery();
-//                searchingForSt(rs, subTopics);
-//            } else if (topic == 0) {
-//                PreparedStatement ps = conn.prepareStatement(buildingSearchStatement(sb, topic, search));
-//                ps.setLong(1, limitStart);
-//                ps.setLong(2, ROWS);
-//                ResultSet rs = ps.executeQuery();
-//                searchingForSt(rs, subTopics);
-//            } else if (search.trim().equals("")){
-//                String customSQL = buildingSearchStatement(sb, topic, search);
-//                ResultSet rs = statement.executeQuery("SELECT * FROM subtopics WHERE topic_id='" + topic + "' LIMIT " + limitStart + ", " + ROWS);
-//                searchingForSt(rs, subTopics);
-//            } else {
-//                search = makeSearchIntoArray(search);
-//                ResultSet rs = statement.executeQuery("SELECT * FROM subtopics WHERE topic_id='" + topic + "' AND " + search + " LIMIT " + limitStart +
-//                        ", " + ROWS);
-//                searchingForSt(rs, subTopics);
-//            }
-
-//            int arraySize = search.trim().split("").length;
-//            if(search.trim().equals("") && topic == 0){
-//            } else if (topic == 0) {
-//                sb.append(search);
-//                sb.append(" LIMIT ?, ?");
-//            } else if (search.trim().equals("")){
-//                sb.append(" WHERE " + SUB_TOPICS_TOPIC_ID + "=?");
-//            } else {
-//                search = makeSearchIntoArray(search);
-//                sb.append(" WHERE " + SUB_TOPICS_TOPIC_ID + "=? AND " + search);
-//            }
             sb.append(SELECT_FROM_SUBTOPICS_START);
             int index = 1;
             String[] array = search.trim().split(" ");
-            for(String test :array){
-                System.out.println(test);
-            }
-            System.out.println(array.length);
             if(topic > 0){
                 sb.append(" WHERE " + SUB_TOPICS_TOPIC_ID + "=?");
             }
@@ -102,7 +63,6 @@ public class SubTopicDAO {
             }
             sb.append(" LIMIT ?, ?");
             PreparedStatement ps = conn.prepareStatement(sb.toString());
-            System.out.println(sb.toString());
             if(topic > 0){
                 ps.setLong(index++, topic);
             }
@@ -131,21 +91,5 @@ public class SubTopicDAO {
             SubTopic subTopic = new SubTopic(id, topicOfId, str);
             st.add(subTopic);
         }
-    }
-
-    static String makeSearchIntoArray(String search){
-        String[] searchArray = search.split(" ");
-        StringBuilder sb = new StringBuilder();
-        sb.append("(");
-//        for(int i = 0; i < searchArray.length; i++){
-//            sb.append(SUB_TOPICS_SUB_TOPIC + " LIKE '%");
-//            sb.append(searchArray[i]);
-//            sb.append("%'");
-//            if(i != searchArray.length - 1){
-//                sb.append(" OR ");
-//            }
-//        }
-        sb.append(")");
-        return sb.toString();
     }
 }
