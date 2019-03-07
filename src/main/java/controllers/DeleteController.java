cb7d1d9545002e971ece86763d7df3731f778a94
Gasolaz <sarunas.slapsys@gmail.com>
2019-03-07 15:31:22 +0200

    neveikia

--- src/main/java/controllers/DeleteController.java
+++ src/main/java/controllers/DeleteController.java
@@ -8,20 +8,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.List;
 
-import static services.AdminService.checkIfExists;
 import static services.AdminService.deleteSubTopic;
 
-@WebServlet("/admin")
-public class AdminController extends HttpServlet {
+
+@WebServlet("/admin/delete")
+public class DeleteController extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.sendRedirect(request.getContextPath());
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        deleteSubTopic(Long.parseLong(request.getParameter("subtopicid")));
 
         List<SubTopic> filteredSubtopics = SubTopicController.searchingService(0, "", 1);
 
@@ -30,10 +32,6 @@ public class AdminController extends HttpServlet {
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(json);
-//        if (checkIfExists(request.getParameter("pass"))) {
-//            request.getRequestDispatcher("build/static/index.html").forward(request, response);
-//        } else {
-//            response.sendRedirect(request.getContextPath());
-//        }
     }
+
 }
--- web/scripts/script.js
+++ web/scripts/script.js
@@ -7,7 +7,7 @@ const state = {
   searchObject: {},
   pages: "",
   path: "StackDocMvn_Web_exploded",
-  password: "123"
+  pass: "123"
 };
 
 const request = async () => {
@@ -208,19 +208,88 @@ const paginationLogic = order => {
 };
 
 const clickLogin = async () => {
-  const response = await fetch(`http://localhost:8080/${state.path}/api/admin`,
+  const response = await fetch(`http://localhost:8080/${state.path}/admin`,
       {
         method: "POST",
-        body: `password=${state.password}`,
+        body: `pass=${state.password}`,
         headers:
             {
               "Content-Type": "application/x-www-form-urlencoded"
             }
       });
 
+  state.subtopics = await response.json();
+
+  document.querySelector('.subtopics').innerHTML = "";
+
+
+  state.subtopics.forEach((subtopic, i) => {
+    const div = document.createElement("div");
+    div.className = "subtopic";
+    div.setAttribute("onclick", `onClick(${subtopic.id})`);
+    document.querySelector('.subtopics').appendChild(div);
+    const topic_name = document.createElement("span");
+    topic_name.className = "topic_name";
+    topic_name.textContent = subtopic.topicId;
+    const subtopic_name = document.createElement("span");
+    subtopic_name.className = "subtopic_name";
+    subtopic_name.textContent = subtopic.subTopic;
+    const subtopic_delete = document.createElement("span");
+    subtopic_delete.className = "subtopic_delete";
+    subtopic_delete.innerHTML = "<i class='fas fa-times'></i>";
+    subtopic_delete.setAttribute("onclick", `clickDelete(${subtopic.id})`);
+    document.querySelectorAll(".subtopic")[i].appendChild(topic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_delete);
+  });
+
+  document.querySelector('.adminpage_logout').style.visibility = "visible";
+  document.querySelector('.adminpage_login').style.visibility = "hidden";
+
 };
 
-const clickLogout = async () => await fetch(`http://localhost:8080/${state.path}/api/admin`);
+const clickLogout = async () => await fetch(`http://localhost:8080/${state.path}/admin`);
+
+const clickDelete = async subtopic_id => {
+  const response = await fetch(`http://localhost:8080/${state.path}/admin/delete`,
+      {
+        method: "POST",
+        body: `subtopicid=${subtopic_id}`,
+        headers:
+            {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+      });
+
+  state.subtopics = await response.json();
+
+  document.querySelector('.subtopics').innerHTML = "";
+
+  state.subtopics.forEach((subtopic, i) => {
+    const div = document.createElement("div");
+    div.className = "subtopic";
+    div.setAttribute("onclick", `onClick(${subtopic.id})`);
+    document.querySelector('.subtopics').appendChild(div);
+    const topic_name = document.createElement("span");
+    topic_name.className = "topic_name";
+    topic_name.textContent = subtopic.topicId;
+    const subtopic_name = document.createElement("span");
+    subtopic_name.className = "subtopic_name";
+    subtopic_name.textContent = subtopic.subTopic;
+    const subtopic_delete = document.createElement("span");
+    subtopic_delete.className = "subtopic_delete";
+    subtopic_delete.innerHTML = "<i class='fas fa-times'></i>";
+    subtopic_delete.setAttribute("onclick", `clickDelete(${subtopic.id})`);
+    console.log(subtopic)
+    subtopic_delete.setAttribute("onmouseover", subtopic.removeAttribute("onclick"));
+    subtopic_delete.setAttribute("onmouseout", subtopic.setAttribute("onclick", `onClick(${subtopic_id})`));
+    document.querySelectorAll(".subtopic")[i].appendChild(topic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_delete);
+  });
+
+
+};
 
 
 
