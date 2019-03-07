7ad3ab886c4c0a20af727e0ebeec2b352041083c
LUNKUS <sigitas@lunkus.com>
2019-03-07 13:06:58 +0200

    delete and insert

---
+++ src/main/java/controllers/AdminController.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static services.AdminService.checkIfExists;
+
+@WebServlet("/admin")
+public class AdminController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        if (checkIfExists(request.getParameter("pass"))) {
+
+            request.getRequestDispatcher("build/static/index.html").forward(request, response);
+
+        } else {
+            response.sendRedirect(request.getContextPath());
+        }
+
+    }
+
+
+}
