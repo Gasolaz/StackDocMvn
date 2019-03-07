9ddb9fd2e12bd9c184ba8389b25da5c4d452d3c5
Arnold <arnold.girich@gmail.com>
2019-03-07 14:18:49 +0200

    implementing delete function

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -7,7 +7,7 @@ const state = {
   searchObject: {},
   pages: "",
   path: "com_Saras_Web_exploded",
-  password: "123"
+  pass: "123"
 };
 
 const request = async () => {
@@ -208,19 +208,82 @@ const paginationLogic = order => {
 };
 
 const clickLogin = async () => {
-  const response = await fetch(`http://localhost:8080/${state.path}/api/admin`,
+  const response = await fetch(`http://localhost:8080/${state.path}/admin`,
       {
         method: "POST",
-        body: `password=${state.password}`,
+        body: `pass=${state.password}`,
         headers:
             {
               "Content-Type": "application/x-www-form-urlencoded"
             }
       });
 
+
+  document.querySelector('.subtopics').innerHTML = "";
+
+
+  state.subtopics.forEach((subtopic, i) => {
+    const div = document.createElement("div");
+    div.className = "subtopic";
+    div.setAttribute("onclick", `onClick(${subtopic.id})`);
+    document.querySelector('.subtopics').appendChild(div);
+    const topic_name = document.createElement("span");
+    topic_name.className = "topic_name";
+    topic_name.textContent = subtopic.topicId;
+    const subtopic_name = document.createElement("span");
+    subtopic_name.className = "subtopic_name";
+    subtopic_name.textContent = subtopic.subTopic;
+    const subtopic_delete = document.createElement("span");
+    subtopic_delete.className = "subtopic_delete";
+    subtopic_delete.innerHTML = "X";
+    document.querySelectorAll(".subtopic")[i].appendChild(topic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_delete);
+  });
+
+  document.querySelector('.adminpage_logout').style.visibility = "visible";
+  document.querySelector('.adminpage_login').style.visibility = "hidden";
+
 };
 
-const clickLogout = async () => await fetch(`http://localhost:8080/${state.path}/api/admin`);
+const clickLogout = async () => await fetch(`http://localhost:8080/${state.path}/admin`);
+
+const clickDelete = async subtopic_id => {
+  const response = await fetch(`http://localhost:8080/${state.path}/admin/delete`,
+      {
+        method: "POST",
+        body: `subtopicid=${subtopic_id}`,
+        headers:
+            {
+              "Content-Type": "application/x-www-form-urlencoded"
+            }
+      });
+
+  state.subtopics = await response.json();
+
+  document.querySelector('.subtopics').innerHTML = "";
+
+  state.subtopics.forEach((subtopic, i) => {
+    const div = document.createElement("div");
+    div.className = "subtopic";
+    div.setAttribute("onclick", `onClick(${subtopic.id})`);
+    document.querySelector('.subtopics').appendChild(div);
+    const topic_name = document.createElement("span");
+    topic_name.className = "topic_name";
+    topic_name.textContent = subtopic.topicId;
+    const subtopic_name = document.createElement("span");
+    subtopic_name.className = "subtopic_name";
+    subtopic_name.textContent = subtopic.subTopic;
+    const subtopic_delete = document.createElement("span");
+    subtopic_delete.className = "subtopic_delete";
+    subtopic_delete.innerHTML = "X";
+    document.querySelectorAll(".subtopic")[i].appendChild(topic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_name);
+    document.querySelectorAll(".subtopic")[i].appendChild(subtopic_delete);
+  });
+
+
+};
 
 
 
